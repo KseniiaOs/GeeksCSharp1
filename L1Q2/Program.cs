62486fa3703cb20b460daea798ddaee09c21d98b
Kseniia Osokina <ksenia.osokina@gmail.com>
2019-02-25 14:59:36 +0300

    test

---
+++ L1Q2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1Q2
+{
+    class Program
+    {   
+        static void Main(string[] args)
+           
+        {
+            double i;
+            Console.WriteLine("Your height:");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your weight:");
+            double m = Convert.ToDouble(Console.ReadLine());
+            i =  m / ( h * h);
+            Console.WriteLine(i);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
